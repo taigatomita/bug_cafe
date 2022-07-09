b75cd89fb784705dffcf6865ab4b8a42c8cfc6d7
taigatomita <tiger101837@gmail.com>
2022-07-09 14:32:06 +0900

    不要な行の削除, 変数名の変更

--- cafe.rb
+++ cafe.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'debug'
-
 DRINKS = [
   { name: 'コーヒー', price: '300' },
   { name: 'カフェラテ', price: '400' },
@@ -27,10 +25,10 @@ def take_order(menus)
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+ordered_drink_number = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+ordered_food_number = take_order(FOODS)
 
-total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
+total = DRINKS[ordered_drink_number][:price].to_i + FOODS[ordered_food_number][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
